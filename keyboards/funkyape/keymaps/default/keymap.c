87e61e8058b7edd0bd4fa4c6af028ee98f0342c9
Prizz <datno111@gmail.com>
2020-12-02 22:49:06 +0700

    Update tap code for encoders

--- keyboards/funkyape/keymaps/default/keymap.c
+++ keyboards/funkyape/keymaps/default/keymap.c
@@ -105,16 +105,16 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         // Volume control
         if (clockwise) {
-            tap_code(KC_VOLU);
+            tap_code(KC_BTN5);
         } else {
-            tap_code(KC_VOLD);
+            tap_code(KC_BTN4);
         }
     } else if (index == 1) {
         // Page up/Page down
         if (clockwise) {
-            tap_code(KC_PGDN);
+            tap_code(KC_DOWN);
         } else {
-            tap_code(KC_PGUP);
+            tap_code(KC_UP);
         }
     }
 }
